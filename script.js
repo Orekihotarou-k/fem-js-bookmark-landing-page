188be67e67e803b0de1e4ddb70d7b16c5aedc724
Levis Kim <levisgramm@gmail.com>
2024-05-19 14:39:54 +0100

    desktop nav fix

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
 // funtion to toggle menu
-const toggleMenuButton = document.querySelector(".nav-toggle--btn")
-const primaryNav = document.querySelector(".primary-nav")
+const toggleMenuButton = document.querySelector(".nav-toggle--btn");
+const primaryNav = document.querySelector(".primary-nav");
 
 function toggleMenu() {
-    primaryNav.classList.toggle("active")
-    toggleMenuButton.classList.toggle("btn-active")
+  primaryNav.classList.toggle("active");
+  toggleMenuButton.classList.toggle("btn-active");
 }
 
-toggleMenuButton.addEventListener("click", toggleMenu)
+toggleMenuButton.addEventListener("click", toggleMenu);
 
 // function to change features
-const featureBtnOne = document.querySelector(".feature-btn-one")
-const featureBtnTwo = document.querySelector(".feature-btn-two")
-const featureBtnThree = document.querySelector(".feature-btn-three")
+const featureBtnOne = document.querySelector(".feature-btn-one");
+const featureBtnTwo = document.querySelector(".feature-btn-two");
+const featureBtnThree = document.querySelector(".feature-btn-three");
 
-// feature elements 
-const featureOne = document.querySelector(".feature-one")
-const featureTwo = document.querySelector(".feature-two")
-const featureThree = document.querySelector(".feature-three")
+// feature elements
+const featureOne = document.querySelector(".feature-one");
+const featureTwo = document.querySelector(".feature-two");
+const featureThree = document.querySelector(".feature-three");
 
-featureBtnTwo.addEventListener("click", toggleFeatureTwo)
+featureBtnTwo.addEventListener("click", toggleFeatureTwo);
 
 function toggleFeatureTwo() {
-    featureOne.classList.add("inactive")
-    featureThree.classList.add("inactive")
-    featureTwo.classList.remove("inactive")
+  featureOne.classList.add("inactive");
+  featureThree.classList.add("inactive");
+  featureTwo.classList.remove("inactive");
 }
 
-featureBtnThree.addEventListener("click", toggleFeatureThree)
+featureBtnThree.addEventListener("click", toggleFeatureThree);
 
 function toggleFeatureThree() {
-    featureTwo.classList.add("inactive")
-    featureOne.classList.add("inactive")
-    featureThree.classList.remove("inactive")
+  featureTwo.classList.add("inactive");
+  featureOne.classList.add("inactive");
+  featureThree.classList.remove("inactive");
 }
 
-featureBtnOne.addEventListener("click", toggleFeatureOne)
+featureBtnOne.addEventListener("click", toggleFeatureOne);
 
 function toggleFeatureOne() {
-    featureTwo.classList.add("inactive")
-    featureThree.classList.add("inactive")
-    featureOne.classList.remove("inactive")
+  featureTwo.classList.add("inactive");
+  featureThree.classList.add("inactive");
+  featureOne.classList.remove("inactive");
 }
 
+// const featureBtns = document.querySelectorAll(".feature-btn");
+// const featureElements = document.querySelectorAll(".feature");
 
-// function to toggle faq 
-const faq = document.querySelectorAll(".faq")
-const faqQuestion = document.querySelectorAll(".faq--question")
+// featureBtns.forEach((btn, index) => {
+//   btn.addEventListener("click", () => {
+//     featureElements.forEach((el, i) => {
+//       if (i === index) {
+//         el.classList.remove("inactive");
+//       } else {
+//         el.classList.add("inactive");
+//       }
+//     });
+//   });
+// });
 
-faqQuestion.forEach(element => {
-    element.addEventListener("click", toggleAnswer)
-    console.log("Im happening")
-})
+// function to toggle faq
+const faq = document.querySelectorAll(".faq");
+const faqQuestion = document.querySelectorAll(".faq--question");
 
-function toggleAnswer() {
-    faq.forEach(item => {
-        item.classList.toggle("faq-active")
-        console.log('im the faq')
-    })
-}
+faqQuestion.forEach((element) => {
+  element.addEventListener("click", function () {
+    this.parentNode.classList.toggle("faq-active");
+  });
+});
